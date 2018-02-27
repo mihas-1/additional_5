699370e27fb2917cb3bd87f897346515815a3158
unknown <loggitec4@ramblerr.ru>
2018-02-28 02:47:16 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	
+    let openSk = [];
+	let sArray = str.split('');
+	
+    for ( let i = 0;  i < str.length; i++) {
+        for ( let j = 0; j < bracketsConfig.length; j++) {
+            if (sArray[i] === bracketsConfig[j][1] && openSk[openSk.length - 1] === bracketsConfig[j][0]) {
+                openSk.pop();  
+            }
+             else if (sArray[i] === bracketsConfig[j][0]) {
+				openSk.push(sArray[i]);
+            }
+        }
+    }
+	return (openSk.length === 0) ? true : false;
 }
