9187a051691432b7d9a5d33fe3c55fd9bd40acdb
unknown <loggitec4@ramblerr.ru>
2018-02-28 18:07:56 +0300

    solution all

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-	
-    let openSk = [];
+    
+	let openSk = [];
 	let sArray = str.split('');
 	
-    for ( let i = 0;  i < str.length; i++) {
-        for ( let j = 0; j < bracketsConfig.length; j++) {
-            if (sArray[i] === bracketsConfig[j][1] && openSk[openSk.length - 1] === bracketsConfig[j][0]) {
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if ( sArray[i] === bracketsConfig[j][1] && openSk[openSk.length - 1] === bracketsConfig[j][0] ) {
                 openSk.pop();  
             }
-             else if (sArray[i] === bracketsConfig[j][0]) {
+              else if ( sArray[i] === bracketsConfig[j][0] ) {
 				openSk.push(sArray[i]);
             }
         }
     }
+	
+	console.log(openSk);
+	if ( (str.length / 2) != Math.floor(str.length/2) ) return false;
 	return (openSk.length === 0) ? true : false;
 }
